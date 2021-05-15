17051ae7171684028693b34389cba7a8f9a4b568
BÉRÉZIAT <thomas.bereziat.externe@capgemini.com>
2021-05-15 12:05:44 +0200

    write build

--- npm.sh
+++ npm.sh
@@ -1,3 +1,3 @@
-sudo apt-get install curl software-properties-common
+sudo apt-get install curl software-properties-common build-essential
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt-get install nodejs
